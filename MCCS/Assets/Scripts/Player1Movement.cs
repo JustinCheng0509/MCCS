9dd0a2ce80cf5c58fccce0d79e328facf9ae1bb2
JustinCheng0509 <Yusi.cheng.0509@gmail.com>
2023-11-02 16:09:35 -0700

    player 2 added

---
+++ MCCS/Assets/Scripts/Player1Movement.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Player1Movement : MonoBehaviour
+{
+    public float MovementSpeed = 5f;
+    public float Jumpforce = 10f;
+    // Start is called before the first frame update
+    private Rigidbody2D _rigidbody;
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        //left & right movement
+        var movement = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
+
+        //Jump
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            _rigidbody.AddForce(Vector2.up * Jumpforce, ForceMode2D.Impulse);
+        }
+    }
+}
--- MCCS/Assets/Scripts/Player2Movement1.cs
+++ MCCS/Assets/Scripts/Player2Movement1.cs
@@ -1,9 +1,9 @@
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class Player2Movement : MonoBehaviour
 {
     public float MovementSpeed = 5f;
-    public float Jumpforce = 1f;
+    public float Jumpforce = 10f;
     // Start is called before the first frame update
     private Rigidbody2D _rigidbody;
     private void Start()
@@ -19,7 +19,7 @@ public class PlayerMovement : MonoBehaviour
         transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
 
         //Jump
-        if (Input.GetKeyDown(KeyCode.W))
+        if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             _rigidbody.AddForce(Vector2.up * Jumpforce, ForceMode2D.Impulse);
         }
