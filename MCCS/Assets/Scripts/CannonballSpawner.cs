3671615358824762b6cdc0302f5dd23f5e6b482b
Raven721 <49292982+Raven721@users.noreply.github.com>
2023-11-27 21:34:24 -0800

    add special skill

---
+++ MCCS/Assets/Scripts/CannonballSpawner.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class CannonballSpawner : MonoBehaviour
+{
+    public GameObject cannonballPrefab; // 炮弹预设
+    public float spawnDistance = 5f; // 生成距离
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            Vector3 spawnPosition = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width / 2, Screen.height, Camera.main.nearClipPlane));
+            spawnPosition.z = 0; // 确保Z坐标为0，因为是2D游戏
+            spawnPosition += Vector3.forward * spawnDistance; // 根据玩家视角前方一定距离生成
+
+            Instantiate(cannonballPrefab, spawnPosition, Quaternion.identity);
+        }
+    }
+
+ }
+
