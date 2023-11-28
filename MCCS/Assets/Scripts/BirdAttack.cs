95df339cb559616da5db2e0d124c7c2c7136ce78
Raven721 <49292982+Raven721@users.noreply.github.com>
2023-11-27 23:37:11 -0800

    fix bug

---
+++ MCCS/Assets/Scripts/BirdAttack.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    public int damage = 10; // 鸟对敌人造成的伤害
+
+    // 设置鸟的速度
+    public void SetVelocity(Vector2 velocity)
+    {
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        if (rb != null)
+        {
+            rb.velocity = velocity;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // 检测碰到的是否是敌人
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            // 对敌人造成伤害
+            collision.gameObject.GetComponent<Enemy>().Damaged(damage, 1);
+        }
+    }
+
+}
